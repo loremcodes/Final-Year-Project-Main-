93758eef79fbfead85a2493fa05ada6d019b73c6
Prasad Junghare <86673407+loremcodes@users.noreply.github.com>
2022-12-06 11:32:20 +0530

    first commit form github

---
+++ HelloWorld/App.js
@@ -0,0 +1,29 @@
+import {StatusBar} from 'expo-status-bar';
+// by paddy -- import button
+import {Button} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Hello floks</Text>
+            <Button className='btn' title="Click me"/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'red',
+        fontSize: '100px',
+    },
+    btn: {
+        border: '2px',
+        color: 'red',
+    }
+});
